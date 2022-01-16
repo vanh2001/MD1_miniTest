7838be4cff6712d44f78bee18c0c6b02acac1293
vanh2001 <anhviet1709@gmail.com>
2022-01-17 01:16:39 +0700

    Update bai check SNT

--- bai4.js
+++ bai4.js
@@ -13,21 +13,12 @@ function checkSNT(n) {
     return check;
 }
 
-function demo2() {
-    let x =1;
-    let i = 0;
+function getTotal() {
     let total = 0;
-    while (x>0){
-        x++;
-        if (checkSNT(x)){
-            document.writeln(x);
-            i++;
-            total += x;
-        }
-        if (x < 500){
-            break;
+    for (let j = 2; j < 500; j++) {
+        if(checkSNT(j) == true){
+            total += j;
         }
     }
-    document.writeln("<br>");
-    document.writeln("Tổng của các SNT nhỏ hơn 500 là: " + total);
+    document.writeln("Tổng của các số nguyên tố nhỏ hơn 500 là: " + total);
 }
